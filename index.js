866676ab33e5cf8ca9f8f7e9240c3822cb681117
cris93g <37388115+cris93g@users.noreply.github.com>
2018-06-27 14:48:50 -0500

    completed

--- index.js
+++ index.js
@@ -1,8 +1,73 @@
-const express = require('express');
-const session = require('express-session');
+require("dotenv").config();
+const session = require("express-session");
+const express = require("express");
+const passport = require("passport");
+const students = require("./students.json");
+const Auth0Strategy = require("passport-auth0");
 
 const app = express();
+app.use(
+  session({
+    secret: "sup dude",
+    resave: false,
+    saveUninitialized: false
+  })
+);
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(
+  new Auth0Strategy(
+    {
+      domain: process.env.DOMAIN,
+      clientID: process.env.client_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      callbackURL: "/login",
+      scope: "openid email profile"
+    },
+    function(accessToken, refreshToken, extraParams, profile, done) {
+      // accessToken is the token to call Auth0 API (not needed in the most cases)
+      // extraParams.id_token has the JSON Web Token
+      // profile has all the information from the user
+      return done(null, profile);
+    }
+  )
+);
 
-const port = 3000;
-app.listen( port, () => { console.log(`Server listening on port ${port}`); } );
+passport.serializeUser((user, done) => {
+  return done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  return done(null, user);
+});
+
+app.get(
+  "/login",
+  passport.authenticate("auth0", {
+    successRedirect: "/me",
+    failureRedirect: "/github",
+    failureFlash: true
+  })
+);
+
+function authenticated(req, res, next) {
+  if (req.user) {
+    next();
+  } else {
+    res.sendStatus(401);
+  }
+}
+
+app.get("/students", (req, res, next) => {
+  if (!req.user) {
+    res.status(500).json({ message: "no user login" });
+  } else {
+    res.status(200).json(req.user);
+  }
+});
+
+const port = 3001;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
